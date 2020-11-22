5323e0c0f46773af8a5d9c5e3f77b6025e055212
Mykal Machon <mykalmachon@gmail.com>
2020-11-22 11:08:17 -0800

    Basic Next.js + Tailwind App

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// This is the home page
+const HomePage = () => {
+  return (
+    <main>
+      <h1 className="text-xl text-gray-900">COMP 390 - Homepage</h1>
+      <p className="text-lg text-gray-600 my-2">This is a website</p>
+    </main>
+  );
+};
+
+export default HomePage;
